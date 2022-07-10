d3debe894480e10709bc3a341852ff6b1b8afe2e
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2022-07-10 12:45:11 +0500

    cors => wip

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 
-const http = require("http")
+const http = require("https")
 const server = http.createServer(app)
 
 // const router = require('./router');
