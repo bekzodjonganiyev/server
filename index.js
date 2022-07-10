aa0f47fa74522bf03030476cda0440e824880548
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2022-07-11 00:51:03 +0500

    make it better

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-'use strict';
+const express = require("express");
+const app = express();
 
-const express = require('express');
-const socketIO = require('socket.io');
+const http = require("http");
+const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3001;
-const INDEX = '/index.html';
+const cors = require("cors");
+app.use(cors());
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
-
-// io.on('connection', (socket) => {
-//   console.log('Client connected');
-//   socket.on('disconnect', () => console.log('Client disconnected'));
-// });
-
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
-
-io.on("connection", (socket) => {
-    console.log("User connected", socket.id)
-
-    socket.on("join_room", (data) => {
-        socket.join(data)
-        console.log(`User with ID ${socket.id} jonied room: ${data}`);
-    })
-
-    socket.on("send_message", data => {
-        socket.to(data.room).emit("receive_message", data)
-    }) 
-
-    socket.on("disconnect", () => {
-        console.log("User disconnected", socket.id)
-    })
+app.get('/', (req, res) => {
+  res.write(`<h1>Socket server is running</h1> `)
+  res.end()
 })
 
-// server.listen(3001, () => {
-//     console.log("Server is running")
-// })
+const { Server } = require("socket.io");
+const io = new Server(
+  server,
+  {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  }
+);
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING")
+})
